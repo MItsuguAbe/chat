36e68be219adb5c516a36b68d71a71e1900426e9
MItsuguAbe <kouiy42@gmail.com>
2020-03-06 18:27:23 +0900

    ログアウト機能

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,6 +5,7 @@
      <p v-for="channel in channels">
        <nuxt-link :to="`/channels/${channel.id}`">{{ channel.name }}</nuxt-link>
      </p>
+     <p v-if="isAuthenticated" class="logout" v-on:click="logout">ログアウト</p>
    </div>
    <div class="main-content">
      <nuxt />
@@ -14,13 +15,36 @@
 
 
 <script>
-import { db } from '~/plugins/firebase'
+import { db, firebase } from '~/plugins/firebase'
+import { mapActions } from 'vuex'
+
 
 export default {
 data () {
     return {
       channels: []
     }
+  },
+
+   computed: {
+      isAuthenticated() {
+        return this.$store.getters.isAuthenticated
+      }
+    },
+
+  methods: {
+   ...mapActions(['setUser']),
+   logout() {
+     firebase.auth().signOut()
+       .then(() => {
+         this.setUser(null)
+         window.alert('ログアウトに成功！')
+       })
+       .catch((e) => {
+         window.alert('ログアウトに失敗しました')
+         console.log(e)
+       })
+   }
   },
 
  mounted () {
@@ -109,6 +133,14 @@ html {
  width: 100%;
  background: #F1F1F1;
  height: 100vh;
+}
+
+
+
+.logout {
+  position: absolute;
+  bottom: 10px;
+  cursor: pointer;
 }
 
 
